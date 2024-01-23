2bbfb7a01ba6a6475b8cc825bb5a13c5f1f45ca6
mateus <mbpereira98@gmail.com>
2024-01-22 21:21:09 -0400

    refact

--- src/PlanningPoker/Domain/Abstractions/Entity.cs
+++ src/PlanningPoker/Domain/Abstractions/Entity.cs
@@ -6,20 +6,20 @@ namespace Domain.Abstractions
     public abstract class Entity<TEntity> : IEntity
         where TEntity : Entity<TEntity>
     {
-        private readonly Validator<TEntity> Validator;
+        private readonly Validator<TEntity> validator;
         private readonly IList<IDomainEvent> _domainEvents = new List<IDomainEvent>();
 
         public int Id { get; protected set; }
 
         public Entity()
         {
-            Validator = new Validator<TEntity>();
-            ConfigureValidationRules(Validator);
+            validator = new Validator<TEntity>();
+            ConfigureValidationRules(validator);
         }
 
         protected abstract void ConfigureValidationRules(Validator<TEntity> validator);
 
-        public virtual ValidationResult Validate() => new ValidationResult(Validator.Validate((TEntity)this));
+        public virtual ValidationResult Validate() => new ValidationResult(validator.Validate((TEntity)this));
 
         public IReadOnlyList<IDomainEvent> GetDomainEvents()
             => _domainEvents.ToList();
