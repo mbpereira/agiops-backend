dc4730378203f70e1d916205fb8d89e234465e93
mateus <mbpereira98@gmail.com>
2024-02-16 11:58:08 -0400

    modificação do script para gerar os resultados na pasta raiz

--- verify-coverage.bat
+++ verify-coverage.bat
@@ -1,5 +1,5 @@
 dotnet tool update dotnet-reportgenerator-globaltool --tool-path ../tools --version 5.2.1
 
-dotnet test ./tests/PlanningPoker/UnitTests /p:CollectCoverage=true /p:CoverletOutput=test-reports/coverage-info/ /p:CoverletOutputFormat=cobertura --no-build --verbosity normal --logger trx --results-directory "test-reports"
+dotnet test ./tests/PlanningPoker/UnitTests /p:CollectCoverage=true /p:CoverletOutput=../../../test-reports/coverage-info/ /p:CoverletOutputFormat=cobertura --no-build --verbosity normal --logger trx --results-directory ./test-reports
 
-"../tools/reportgenerator.exe" -reports:./tests/PlanningPoker/UnitTests/test-reports/coverage-info/coverage.cobertura.xml -targetdir:./test-reports/coverage-report
+"../tools/reportgenerator.exe" -reports:./test-reports/coverage-info/coverage.cobertura.xml -targetdir:./test-reports/coverage-report
